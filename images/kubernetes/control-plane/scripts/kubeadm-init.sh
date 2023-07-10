2cae323883170c321d6a4f6134cf5d0224251253
Aiden De Loryn <aiden.deloryn@tensorworks.com.au>
2023-07-11 09:36:08 +1000

    Refactor Vagrantfile to incorporate variables for configuration

--- images/kubernetes/control-plane/scripts/kubeadm-init.sh
+++ images/kubernetes/control-plane/scripts/kubeadm-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo kubeadm init --kubernetes-version ${K8S_VERSION} --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=${NODE_IP}
+sudo kubeadm init --kubernetes-version ${K8S_VERSION} --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=${CP_NODE_IP}
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
